8fd50777ef053339f02f1532408671189aa4dd3c
robert <matthewrueter51@gmail.com>
2022-10-06 15:13:33 -0500

    create data directory with questions file for fetching question content from API.

---
+++ src/data/questions.js
@@ -0,0 +1,12 @@
+
+export var questions = fetch('https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data[0].question));
+
+export var choices = fetch('https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data[0].choices));
+
+export var correctAnswer = fetch('https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data[0].answer));
