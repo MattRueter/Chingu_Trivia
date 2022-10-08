7d01d5938ec66cb4cd5124f3b054fc655809fd7e
robert <matthewrueter51@gmail.com>
2022-10-08 11:15:03 -0500

    Container (parent) passes question as prop to Question (child).

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {questions} from '../data/questions.js';
 import Counter from './Counter.js';
@@ -6,12 +6,13 @@ import Question from './Question.js';
 import Options from './Options.js';
 
 export default function Container (){
-
+	const [question, setQuestion] = useState("");
+	questions(setQuestion)
 
 	return (
 		<div id="container">
 			<Counter />
-			<Question />
+			<Question question={question} />
 			<Options />
 			<button>NEXT</button>
 		</div>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,6 @@
 import React, {useState} from 'react';
-import {questions} from '../data/questions.js';
 
-
-//questions call below is testing 'consumption' of async code which 
-//fetches API info. See Questions.js file.
-
-
-export default function Question() {
-  const [question, setQuestion] = useState("");
-
-  questions(setQuestion)
+export default function Question({question}) {
   
   return (
    <h2>{question}</h2>
