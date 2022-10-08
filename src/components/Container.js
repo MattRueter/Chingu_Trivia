06b131367d2cd05a71d1d8a2485085d8e30d9e05
robert <matthewrueter51@gmail.com>
2022-10-08 11:08:38 -0500

    Import 'questions' function from questions.js database ahead of chaniging this component to manage all state of App.

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import {questions} from '../data/questions.js';
 import Counter from './Counter.js';
 import Question from './Question.js';
 import Options from './Options.js';
 
 export default function Container (){
+
+
 	return (
 		<div id="container">
 			<Counter />
