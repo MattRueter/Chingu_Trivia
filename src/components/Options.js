d10510b3622010fda1f86ec6cfba9719cb33b2b3
robert <matthewrueter51@gmail.com>
2022-10-11 17:33:14 -0500

    add styles to Option li.

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Options ({options}){
 		
 	return (
-		<div>
+		<div id="optionsBox">
 			{options.map((option, index)=>{
 				return (			
-					<div key={index}>{`${option[0]}:    ${option[1]}`} </div>		
+					<div className="optionItem" key={index}>{`${option[0]}: ${option[1]}`} </div>		
 				)
 			})}
 		</div>
