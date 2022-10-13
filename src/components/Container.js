92bfda92d58aac9e72622b7c7fc7727ddff6a5a2
robert <matthewrueter51@gmail.com>
2022-10-13 18:08:48 -0500

    next button updates entire Container component.

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,19 +6,35 @@ import Question from './Question.js';
 import Options from './Options.js';
 
 export default function Container (){
-
-	const [currentQuestionObj, setCurrentQuestionObj] = useState(questions[0])
-	const [questionNumber, setQuestionNumber] = useState(currentQuestionObj.id)
+	
 	let totalQuestions = questions.length;
-	const [question, setQuestion] = useState(currentQuestionObj.question)	
-	const [options, setOptions] = useState(Object.entries(currentQuestionObj.choices))	
+	
+	const [questionNumber, setQuestionNumber] = useState(1)
+	const [question, setQuestion] = useState(questions[0].question)	
+	const [options, setOptions] = useState(Object.entries(questions[0].choices))	
+
+
+
+	function handleClick (){
+		const index = questions.findIndex(item =>{
+			return item.question===question
+		})
+		console.log(index);
+
+		const nextIndex = index +1
 		
+
+		setQuestionNumber(nextIndex+1);
+		setQuestion(questions[nextIndex].question);
+		setOptions(Object.entries(questions[nextIndex].choices))
+	};
+
 	return (
 		<div id="container">
 			<Counter questionNumber={questionNumber} totalQuestions={totalQuestions} />
 			<Question question={question} />
 			<Options options={options}/>
-			<button>NEXT</button>
+			<button onClick={handleClick}>NEXT</button>
 		</div>
 	)
 }
