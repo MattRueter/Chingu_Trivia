673664893502ad1ca6a51f7b98b292e3e556cb6f
robert <matthewrueter51@gmail.com>
2022-10-13 17:04:49 -0500

    hard write API data into array for the moment.

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,60 +1,24 @@
 import React, {useState, useEffect} from 'react';
 
-
+import {questions} from '../data/questions.js';
 import Counter from './Counter.js';
 import Question from './Question.js';
 import Options from './Options.js';
 
 export default function Container (){
-	const [questionsArray, setQuestionsArray] = useState(null)
-	const [question, setQuestion] = useState("question")	
-	const [options, setOptions] = useState([])	
-	const [questionNumber, setQuestionNumber] = useState("one")
-	
-	useEffect(() => {
-		fetch('https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json')
-		.then((response) =>response.json())
-		.then((data) => {
-		 setQuestionsArray([...data])
-		 updateCard(0)
-		})	
-	},[])
 
-	console.log(questionsArray)
-
-	function updateCard(index){
-		setQuestion(questionsArray[index].question)
-		setOptions(Object.entries(questionsArray[index].choices) )
-		setQuestionNumber(questionsArray[index].id)
-	}
-	
-	
+	const [currentQuestionObj, setCurrentQuestionObj] = useState(questions[0])
+	const [questionNumber, setQuestionNumber] = useState(currentQuestionObj.id)
+	let totalQuestions = questions.length;
+	const [question, setQuestion] = useState(currentQuestionObj.question)	
+	const [options, setOptions] = useState(Object.entries(currentQuestionObj.choices))	
+		
 	return (
 		<div id="container">
-			<Counter questionNumber={questionNumber} totalQuestions={45} />
+			<Counter questionNumber={questionNumber} totalQuestions={totalQuestions} />
 			<Question question={question} />
 			<Options options={options}/>
 			<button>NEXT</button>
 		</div>
 	)
-
 }
-
-
-/*
-const [options, setOptions] = useState([])
-optionSet(setOptions)
-
-const [questionNumber, setQuestionNumber] = useState([])	
-counter(setQuestionNumber);
-*/
-
-
-/*
-<div id="container">
-			<Counter questionNumber={questionNumber} totalQuestions={45} />
-			<Question question={question} />
-			<Options options={options}/>
-			<button>NEXT</button>
-		</div>
-*/
