1b5f603b86bdeda77a57d6aaca3da1114ba76360
robert <matthewrueter51@gmail.com>
2022-10-16 16:43:32 -0500

    wrap current user stats inside of element in Nav.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,11 +5,13 @@ export default function Nav ({user,correct,incorrect}){
 	
 	
 	return(
-		<nav id="navbar">
-			<h1>Chingu Trivia</h1>
-			<h2>{user.userName}</h2>
-			<div>Correct: {correct}</div>
-			<div>Incorrect: {incorrect}</div>
+		<nav id="navbar">		
+				<h1>Chingu Trivia</h1>		
+			<div className="navWrapper">
+				<h2>{user.userName}</h2>
+				<div>Correct: {correct}</div>
+				<div>Incorrect: {incorrect}</div>
+			</div>
 		</nav>
 	)
 }
