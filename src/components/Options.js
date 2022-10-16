e58a15bd5ba5a43fd4bbdd40e9976e283e298a1d
robert <matthewrueter51@gmail.com>
2022-10-16 11:13:09 -0500

    add click handler to determine if selected option is correct or not.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
-export default function Options ({options}){
-		
+export default function Options ({options, answer}){
+
+function selectAnswer (selectedChoice){
+	if(selectedChoice === answer){		
+		console.log("correct!")
+	}else{
+		console.log("incorrect")
+	}
+};
 	return (
 		<div id="optionsBox">
 			{options.map((option, index)=>{
+				
 				return (			
-					<div className="optionItem" key={index}>{`${option[0]}: ${option[1]}`} </div>		
+					<div className="optionItem" 
+					key={index}
+					onClick={()=>selectAnswer(option[0])}
+					> {`${option[0]}: ${option[1]}`} </div>		
 				)
+				
 			})}
 		</div>
 	)
